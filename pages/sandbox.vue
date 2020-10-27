683c196b23c67b89980868a95b86215794fd36b2
Simon Brandt <simon_brandt@gmx.de>
2020-10-27 12:23:40 +0100

    CHANGE sandbox.vue: SETUP test notifications

--- pages/sandbox.vue
+++ pages/sandbox.vue
@@ -1,6 +1,7 @@
 <template>
     <div>
         <h1>Sandbox</h1>
+        <v-btn @click="askForNotificationPermission">Enable notifications</v-btn>
         <div v-if="users">
             <div v-for="user in users" :key="user.id">
                 updated
@@ -26,6 +27,31 @@ export default {
     },
     async created() {
         this.users = await fetchUsers()
+    },
+    methods: {
+        askForNotificationPermission() {
+            if (window.Notification) {
+                Notification.requestPermission(result => {
+                    if (result === 'granted') {
+                        if (navigator.serviceWorker) {
+                            console.log('navigator.serviceWorker.ready:', navigator.serviceWorker.ready)
+                            navigator.serviceWorker.ready
+                                .then(serviceWorkerRegistration => {
+                                    console.log(serviceWorkerRegistration)
+                                    serviceWorkerRegistration.showNotification('Notification from SW')
+                                })
+                        }
+                        // new Notification('You are subscribed to notifications from this site.', {
+                        //     body: 'Thanks.',
+                        // })
+                    } else {
+                        console.log('Notification permission denied')
+                    }
+                })
+            } else {
+                console.log('Notifications not supported')
+            }
+        },
     },
     components: [user],
 }
